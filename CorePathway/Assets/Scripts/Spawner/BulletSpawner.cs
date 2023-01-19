be228fcc2a894ca2ee24464752b32cba3246c73f
Mikael Gelle <h.gelle@surreyschools.ca>
2023-01-19 10:20:47 -0800

    FirstGit

---
+++ CorePathway/Assets/Scripts/Spawner/BulletSpawner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject bullet;
+
+    public float spawnTime;
+    public float spawnDelay;
+    public float destroyDelay;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnBullet", spawnTime, spawnDelay);
+    }
+
+    public void SpawnBullet()
+    {
+        Instantiate(bullet, transform.position, transform.rotation);
+    }    
+}
